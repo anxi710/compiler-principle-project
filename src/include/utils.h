94f06250ee88dbb52beae274541deeda49bc227f
anxi <anxi710@outlook.com>
2025-03-31 17:39:53 +0800

    转用 c++ 开发

--- src/include/utils.h
+++ src/include/utils.h
@@ -2,15 +2,30 @@
 #define UTILS_H
 
 /* 本文件给出一系列实用函数的声明 */
+#include <stdio.h>
+#include <stdbool.h>
 
-#include "token.h"
+/*----------- Token ----------*/
+struct Token;
 
 /**
- * @brief  向 fd 指定的设备格式化打印/写入一个 Token
- * @param  fd    文件描述符
+ * @brief  向指定文件句柄格式化打印/写入一个 Token
+ * @param  out   输出文件句柄
  * @param  token 词法单元
  * @return void
  */
-void printToken(int fd, struct Token token);
+void printToken(FILE* out, struct Token token);
+
+/*----------- Token ----------*/
+
+/*---------- Lexer ----------*/
+
+// 在 <ctype.h> 中定义了 int isspace(int c) 函数，因此删除了 isWhiteSpace() 函数
+
+/*---------- Lexer ----------*/
+
+/*---------- Parser ----------*/
+
+/*---------- Parser ----------*/
 
 #endif
